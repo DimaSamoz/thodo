5aaf08a23f20ceb2d3d4521abfe6b1ab3726d898
Dima Szamozvancev <ds709@cam.ac.uk>
2016-08-31 17:27:45 +0100

    Restructure project
    Separate library and executable.

---
+++ exe/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Types
+import Commands.Parser
+import Options.Applicative
+
+greet :: Command -> IO ()
+greet c = print c
+
+
+
+main :: IO ()
+main = execParser opts >>= greet
+  where
+    opts = info (helper <*> parseCommand)
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> Options.Applicative.header "hello - a test for optparse-applicative" )
