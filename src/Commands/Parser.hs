9efacb64f3deade8820105d0b46bd94e154ec2f0
Dima Szamozvancev <ds709@cam.ac.uk>
2016-08-31 18:59:24 +0100

    Add Commands.What module

--- src/Commands/Parser.hs
+++ src/Commands/Parser.hs
@@ -7,24 +7,31 @@ import Options.Applicative
 import Types
 import Data.Time hiding (parseTime)
 import Commands.Add
+import Commands.What
 
 data Command
-    = Add TodoTask Category Priority AddItems (Maybe DayOfWeek) (Maybe Day) (Maybe TimeOfDay) deriving (Eq, Show)
+    = Add TodoTask Category Priority AddItems (Maybe DayOfWeek) (Maybe Day) (Maybe TimeOfDay)
+    | What [Category] deriving (Eq, Show)
 
 
 parseAdd :: Parser Command
 parseAdd = Add
     <$> parseTask
-    <*> parseCategory
+    <*> parseAddCategory
     <*> parsePriority
     <*> parseAddItems
     <*> parseDayOfWeek
     <*> parseDate
     <*> parseTime
 
+parseWhat :: Parser Command
+parseWhat = What
+    <$> parseWhatCategory
+
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
 parseCommand :: Parser Command
 parseCommand = subparser $
-    command "add"   (parseAdd   `withInfo` "Add a new TODO-TASK to the list")
+       command "add"   (parseAdd   `withInfo` "Add a new TODO-TASK to the list")
+    <> command "what"  (parseWhat `withInfo` "See the tasks from the given categories")
