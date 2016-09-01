83af3c80b018078932e11e8f8e89ec61a94d3f5a
Dima Szamozvancev <ds709@cam.ac.uk>
2016-09-01 09:42:30 +0100

    Add Commands.Clear module

---
+++ src/Commands/Clear.hs
@@ -0,0 +1,24 @@
+-- | Subparsers for the 'clear' command.
+module Commands.Clear
+    ( parseClearTarget
+    ) where
+
+import Types
+import Options.Applicative
+
+ticked :: Parser String
+ticked = flag' "ticked"
+     ( long "ticked"
+    <> short 't'
+    <> help "Clear ticked tasks")
+
+allTasks :: Parser String
+allTasks = flag' "all"
+     ( long "all"
+    <> short 'A'
+    <> help "Clear all tasks")
+
+
+-- | Parses the category of the task.
+parseClearTarget :: Parser String
+parseClearTarget = ticked <|> allTasks
